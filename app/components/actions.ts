1b7ae00f2c5f68be6e62dd9a5e0ac6f17591a0ee
Mateusz Piwowarski <skyperpl@gmail.com>
2025-01-06 15:12:12 +0100

    Changed requests and modified UI

--- app/components/actions.ts
+++ app/components/actions.ts
@@ -1,34 +1,38 @@
 import { PRIME_NUMBER } from '../constants';
-import { shamir } from './helpers';
 
 export const sendInitialData = async (
   servers: string[],
+  setInitialValuesError: React.Dispatch<React.SetStateAction<string>>,
   formData: FormData,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-): Promise<any> => {
-  const t = Number(formData.get('t')?.toString());
-  const n = Number(formData.get('n')?.toString());
+): Promise<void> => {
+  const t = Number(formData.get('t'));
+  const n = Number(formData.get('n'));
 
-  const promises = servers.entries().map(
-    ([i, server]) =>
+  const promises = servers.map(
+    (server, i) =>
       new Promise((resolve) => {
         fetch(`${server}/api/initial-values`, {
           method: 'POST',
           headers: {
-            'content-type': 'application/json',
+            'Content-Type': 'application/json',
             Accept: 'application/json',
           },
           body: JSON.stringify({
-            t: t,
-            n: n,
+            t,
+            n,
             id: i + 1,
             p: PRIME_NUMBER,
             parties: servers,
           }),
-        }).then((res) => {
-          resolve(res.body);
+        }).then(async (res) => {
+          const data = await res.json();
+          if (!res.ok) {
+            setInitialValuesError(data.detail);
+          }
+          resolve(data);
         });
       }),
   );
+
   await Promise.all(promises);
 };
